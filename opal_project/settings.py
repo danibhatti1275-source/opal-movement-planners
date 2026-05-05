983c9c363446a6f406ff430edbb815182f422cdd
Dani Bhatti <danibhatti1275@gmail.com>
2026-05-05 05:49:13 -0700

    update urls.py and settings.py debug =true

--- opal_project/settings.py
+++ opal_project/settings.py
@@ -75,7 +75,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/opt/render/project/src/media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
